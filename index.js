688842eb600f8060e1c12e22871f509dd192aa6f
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-05-24 15:26:33 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let person = {
+    name: 'Obed',
+    age: 34
+
+};
+//dot notation
+person.age = 67;
+//bracket notation
+person['name'] = 'Jude';
+
+//array
+let selectedColor = ['red', 'blue'];
+console.log(selectedColor);
